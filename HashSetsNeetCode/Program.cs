6034490a5d1fc0d894684736258b1e13fc9b39c7
Özgür KILIÇ <ozgurkilic147@gmail.com>
2025-07-11 19:35:16 +0200

    Commonly Used Methods Practised.

--- HashSetsNeetCode/Program.cs
+++ HashSetsNeetCode/Program.cs
@@ -7,7 +7,7 @@ class Program
         //HashSets
         //HashSet in C# is an unordered collection of unique elements.It is generally used when we want to prevent duplicate elements from being placed in the collection.
         //The performance of the HashSet is much better in comparison to the list. 
-        HashSet<int> hashSet = new();
+        /*HashSet<int> hashSet = new();
         
         //Adding Elements
         hashSet.Add(1);
@@ -46,6 +46,69 @@ class Program
         list.Remove(0);
         hashSet.ExceptWith(list);
         foreach (var VARIABLE in hashSet)
-            Console.WriteLine(VARIABLE);
+            Console.WriteLine(VARIABLE);*/
+        
+        
+        //Method Exercise
+        //Scenerio;
+        /*You are given two student clubs: the Chess Club and the Music Club. Each club stores member names using a HashSet<string>. 
+        Your task is to perform various set operations and demonstrate commonly used HashSet methods.*/
+        /*Tasks:
+        1. Initialize two HashSet<string> instances with initial members.
+        2. Add a new student to each club.
+        3.Check if a particular student is a member of each club.
+        4.Find students who are in both clubs.
+        5.Find students who are in either club.
+        6.Find students who are in Chess Club but not in Music Club.
+        7.Compare if both clubs have the same members.
+        8.Remove a student from a club.
+        9.Clear one club.
+        10.Print results at each step.*/
+        /*//1-
+        HashSet<string>  _chessClub= new();
+        HashSet<string>  _musicClub= new();
+        //2-
+        _chessClub.Add("Carla");
+        _chessClub.Add("Carl");
+        _chessClub.Add("Candice");
+        _chessClub.Add("Greg");
+        _musicClub.Add("Candice");
+        _musicClub.Add("Sarah");
+        _musicClub.Add("Bojack");
+        _musicClub.Add("Greg");
+        //3-
+        Console.WriteLine(_chessClub.Contains("Carla"));
+        Console.WriteLine(_musicClub.Contains("Carla"));
+        //4-
+        HashSet<string> temp = new();
+        temp.UnionWith(_musicClub);
+        temp.IntersectWith(_musicClub);
+        foreach (var VARIABLE in temp)
+        Console.Write(VARIABLE + " ");
+        Console.WriteLine();
+        temp.Clear();
+        //5-
+        temp.UnionWith(_chessClub);
+        temp.UnionWith(_musicClub);
+        foreach (var VARIABLE in temp)
+            Console.Write(VARIABLE + " ");
+        Console.WriteLine();
+        //6-
+        temp.ExceptWith(_chessClub);
+        foreach (var VARIABLE in temp)
+            Console.Write(VARIABLE + " ");
+        Console.WriteLine();
+        //7-
+        Console.WriteLine(_chessClub.SetEquals(_musicClub));
+        //8-
+        _chessClub.Remove("Carla");
+        foreach (var VARIABLE in _chessClub)
+            Console.Write(VARIABLE + " ");
+        //9-
+        _musicClub.Clear();
+        //10-
+        //Done*/
     }
+    
+    
 }
--- HashSetsNeetCode/obj/Debug/net8.0/HashSetsNeetCode.AssemblyInfo.cs
+++ HashSetsNeetCode/obj/Debug/net8.0/HashSetsNeetCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HashSetsNeetCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+461aee5cc1f847f0984b3069b8eace06975a5a61")]
 [assembly: System.Reflection.AssemblyProductAttribute("HashSetsNeetCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HashSetsNeetCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
